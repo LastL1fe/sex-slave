12861426ccd68a5265bd7a95e3cc50d13d117400
LastL1fe <jarellh98@gmail.com>
2022-02-23 16:04:24 -0500

    sex slave

--- shit.js
+++ shit.js
@@ -1,3 +1,26 @@
-const discord = require("discord.js")
+const {Client, Intents} = require("discord.js");
+const {token} = require('./config.json');
 
-const Client = new discord.Client
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const prefix = "!crack"
+const channelId = "724675479017226312"
+
+const commands = ["ccd", "why"]
+
+client.once('ready', () => {
+    console.log("sex slave");
+});
+
+client.on('messageCreate', message => {
+    if (message.content.startsWith(prefix + " ccd")) {
+        message.channel.send('test for count down');
+    }
+});
+
+client.on('messageCreate', message => {
+    if (message.content.startsWith(prefix + " why")) {
+        message.channel.send('fuck off');
+    }
+});
+
+client.login(token);
