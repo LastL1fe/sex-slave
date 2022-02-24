2fff091a9777099c8f500c4323e7af699a2c3bfc
LastL1fe <jarellh98@gmail.com>
2022-02-23 19:17:42 -0500

    Merge branch 'master' of https://github.com/LastL1fe/sex-slave

--- shit.js
+++ shit.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const {Client, Intents} = require("discord.js");
 const {token} = require('./config.json');
 
@@ -7,19 +8,33 @@ const channelId = "724675479017226312"
 
 const commands = ["ccd", "why"]
 
+let onGoingCountDowns = []
+
 client.once('ready', () => {
     console.log("sex slave");
 });
 
 client.on('messageCreate', message => {
-    if (message.content.startsWith(prefix + " ccd")) {
-        message.channel.send('test for count down');
+    if (message.author.id != 875180300395757568){
+        if (message.content.startsWith(prefix + " cct")) {
+            let parsedCode = message.content.split(message.content.indexOf("t" + 1))
+            let countDownDay = new Date(parsedCode);
+            let currentDay = Date()
+
+            message.channel.send(countDownDay.toDateString());
+        }
     }
 });
 
 client.on('messageCreate', message => {
     if (message.content.startsWith(prefix + " why")) {
         message.channel.send('fuck off');
+    }
+});
+
+client.on('messageCreate', message => {
+    if (message.content.startsWith(prefix)){
+        message.channel.send("crack these nuts");
     }
 });
 
